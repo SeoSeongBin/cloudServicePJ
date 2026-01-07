eff449b78b2b28e1c2d757b31dd41eeaf5d8bccd
SeoSeongBin <dhsb123@naver.com>
2026-01-07 11:32:17 +0900

    로그인 세션 확인 및 세션에 따른 페이지 처리

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import React, {useEffect} from "react";
 import { BrowserRouter, Routes, Route, Navigate, useNavigate,Outlet } from "react-router-dom";
 
 function Main() {
+
+    const navigate = useNavigate();
+
     function getSession() {
         // eslint-disable-next-line react-hooks/rules-of-hooks
         useEffect(() => {
@@ -9,8 +12,19 @@ function Main() {
                 method: 'GET',
                 credentials: 'include', // ✅ 쿠키 포함
             })
-                .then(res => res.json())
+                .then(res => {
+                    // 401 Unauthorized인 경우 바로 로그인 페이지로 이동
+                    if (res.status === 401) {
+                        navigate("/login");
+                        return null; 
+                    }
+                    if (!res.ok) throw new Error("시스템 오류");
+                    return res.json();
+                })
                 .then(data => {
+                    if(data == null){
+                        navigate("/login");
+                    }
                     console.log(data);
                 });
             }, []);
